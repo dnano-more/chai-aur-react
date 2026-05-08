e3b3b166952e3feb412434b72dfbceec17c88e5d
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-05-08 19:13:32 +0530

    learn: explore React setup with CRA and Vite

---
+++ 01-basic-react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>chai aur react || Dnyaneshwar More - laerning reactjs from hitesh sir's yt react js seris</h1>
+  );
+}
+
+export default App;
---
+++ 01-vite-react/src/App.jsx
@@ -0,0 +1,14 @@
+function App() {
+
+  return (
+    <>
+      <h1>
+        chai aur react || Dnyaneshwar More - laerning reactjs from hitesh sir's
+        yt react js seris
+      </h1>
+      <h3>this project is created using vite bundler</h3>
+    </>
+  );
+}
+
+export default App;
