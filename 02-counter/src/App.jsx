030e82b9c65f21dea4065a857eccf0208ee773d3
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-05-14 13:24:15 +0530

    create counter app using useState hook of react

---
+++ 02-counter/src/App.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  let [count, setCount] = useState(10);
+
+  const addValue = () => {
+    count = count + 1;
+    if(count > 20) {
+      count = 20
+    }
+
+    setCount(count)
+  };
+  
+  const removeValue = () => {
+    count = count - 1;
+    if(count < 0) {
+      count = 0
+    }
+
+    setCount(count)
+  }
+
+  return (
+    <>
+      <h1>dnano's react counter app</h1>
+      <h3>Counter value: {count}</h3>
+      <br />
+      <button onClick={addValue} className="counter">
+        Increase Value
+      </button>
+      <button onClick={removeValue} className="counter">Decrease Value</button>
+    </>
+  );
+}
+
+export default App;
