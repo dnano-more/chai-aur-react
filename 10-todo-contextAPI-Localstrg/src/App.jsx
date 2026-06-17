223f3a3c67f55dddef5674ee0b1193db04999f08
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-06-17 23:58:36 +0530

    feat: implement Todo context with provider and initial state

--- 10-todo-contextAPI-Localstrg/src/App.jsx
+++ 10-todo-contextAPI-Localstrg/src/App.jsx
@@ -1,12 +1,29 @@
 import { useState } from "react";
+import { TodoProvider } from "./contexts";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [todos, setTodos] = useState([]);
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [...prev])
+  }
 
   return (
-    <>
-      <h1 class="text-3xl font-bold underline bg-blue-400">Hello world!</h1>
-    </>
+    <TodoProvider
+      value={{ todos, addTodo, updatedTodo, deleteTodo, toggleComplete }}
+    >
+      <div className="bg-[#172842] min-h-screen py-8">
+        <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+          <h1 className="text-2xl font-bold text-center mb-8 mt-2">
+            Manage Your Todos
+          </h1>
+          <div className="mb-4">{/* Todo form goes here */}</div>
+          <div className="flex flex-wrap gap-y-3">
+            {/*Loop and Add TodoItem here */}
+          </div>
+        </div>
+      </div>{" "}
+    </TodoProvider>
   );
 }
 
--- 10-todo-contextAPI-Localstrg/src/contexts/TodoContext.js
+++ 10-todo-contextAPI-Localstrg/src/contexts/TodoContext.js
@@ -1,10 +1,22 @@
 import { createContext, useContext } from "react";
 
-export const TodoContext = createContext({});
+export const TodoContext = createContext({
+    todos: [
+        {
+            id: 1,
+            todo: "Learn React",
+            completed: false
+        }
+    ],
+    addTodo: (todo) => {},
+    updatedTodo: (id, todo) => {},
+    deleteTodo: (id) => {},
+    toggleComplete: (id) => {}
+});
 
 export const useTodo = () => {
     return useContext(TodoContext)
 }
 
-export const Todoprovider = TodoContext.Provider
+export const TodoProvider = TodoContext.Provider
 
