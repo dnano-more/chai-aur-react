a8a98be271f2e915c6688f1bb9134da9006b7465
Dnyaneshwar More <dnyaneshwarmore2500@gmail.com>
2026-07-08 22:52:12 +0530

    feat: integrate Redux Toolkit for Todo app with Tailwind CSS styling
    - Added Redux Toolkit dependencies and configured store.
    - Created todoSlice for managing todos with add and remove functionality.
    - Implemented AddTodo and Todos components for adding and displaying todos.
    - Updated App component to include AddTodo and Todos components.
    - Integrated Tailwind CSS for styling and updated index.css.
    - Modified main.jsx to wrap App with Redux Provider for state management.
    - Updated vite.config.js to include Tailwind CSS plugin.

--- reduxToolkitTodo/src/App.jsx
+++ reduxToolkitTodo/src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import { useState } from "react";
+import AddTodo from "./components/AddTodo";
+import Todos from "./components/Todos";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
